fbcca959a969334af3378c4aad269a188e454eb1
guillermina <manzanog0612@gmail.com>
2021-09-17 00:44:42 -0300

    Added basic entity functions.

--- Motor-Grafico/src/Motor/entity.cpp
+++ Motor-Grafico/src/Motor/entity.cpp
@@ -11,4 +11,58 @@ namespace engine
 	{
 
 	}
+
+	void entity::UpdateModelMatrix()
+	{
+		model = translate * rotateX * rotateY * rotateZ * scale;
+	}
+
+	void entity::SetRotX(float x)
+	{
+		v3rot[0] = x;
+
+		glm::vec3 axis;
+
+		axis[1] = axis[2] = 0.0f;
+		axis[0] = 1.0f;
+
+		rotateX = glm::rotate(glm::mat4(1.0f), x, axis);
+		UpdateModelMatrix();
+	}
+
+	void entity::SetRotY(float y)
+	{
+		v3rot[1] = y;
+
+		glm::vec3 axis;
+
+		axis[0] = axis[2] = 0.0f;
+		axis[1] = 1.0f;
+
+		rotateY = glm::rotate(glm::mat4(1.0f), y, axis);
+		UpdateModelMatrix();
+	}
+
+	void entity::SetRotZ(float z)
+	{
+		v3rot[2] = z;
+
+		glm::vec3 axis;
+
+		axis[0] = axis[1] = 0.0f;
+		axis[2] = 1.0f;
+
+		rotateZ = glm::rotate(glm::mat4(1.0f), z, axis);
+		UpdateModelMatrix();
+	}
+
+	void entity::SetScale(float x, float y, float z)
+	{
+		v3scale[0] = x;
+		v3scale[1] = y;
+		v3scale[2] = z;
+
+		translate = glm::translate(glm::mat4(1.0f), v3pos);
+		UpdateModelMatrix();
+	}
 }
--- Motor-Grafico/src/Motor/entity.h
+++ Motor-Grafico/src/Motor/entity.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "exports.h"
+#include "renderer.h"
 
 namespace engine
 {
@@ -8,6 +9,27 @@ namespace engine
 	public:
 		entity();
 		~entity();
+
+	protected:
+		renderer* renderer;
+
+		glm::vec3 v3pos;
+		glm::vec3 v3rot;
+		glm::vec3 v3scale;
+
+		glm::mat4 model;
+
+		glm::mat4 translate;
+		glm::mat4 rotateX;
+		glm::mat4 rotateY;
+		glm::mat4 rotateZ;
+		glm::mat4 scale;
+
+		void UpdateModelMatrix();
+		void SetRotX(float x);
+		void SetRotY(float y);
+		void SetRotZ(float z);
+		void SetScale(float x, float y, float z);
 
 	private:
 
