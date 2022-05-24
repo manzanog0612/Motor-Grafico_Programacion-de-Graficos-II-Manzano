e6660da299bed1ea643fa74961a87c9b986b2ffa
Guillermina Manzano <guillerminamanzano@pathfindersstudio.com>
2022-05-24 08:15:46 -0300

    Hotfix.

--- Motor-Grafico/src/Motor/sprite.cpp
+++ Motor-Grafico/src/Motor/sprite.cpp
@@ -226,9 +226,9 @@ namespace engine
 		_renderer = render;
 		float vertex[24] =
 		{
-			 0.5f,  0.5f, 0.0f,		0.0f, 0.0f,1.0f,
-			 0.5f, -0.5f, 0.0f,		0.0f, 0.0f,1.0f,
-			-0.5f, -0.5f, 0.0f,		0.0f, 0.0f,1.0f,
+			 0.5f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f,
+			 0.5f, -0.5f, 0.0f,		0.0f, 0.0f, 1.0f,
+			-0.5f, -0.5f, 0.0f,		0.0f, 0.0f, 1.0f,
 			-0.5f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f
 		};
 		unsigned int indices[6] =
